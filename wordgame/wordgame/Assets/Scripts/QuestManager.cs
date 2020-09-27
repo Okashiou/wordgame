df1588bd1a7c8bc71bdf1014815aed2babd76434
asuka <kawaguchiasuka0111@yahoo.co.jp>
2020-09-27 13:39:08 +0300

    add assets

---
+++ wordgame/wordgame/Assets/Scripts/QuestManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public StageUIManager stageUI;
+
+    int[] encountTable = { -1, -1, 0, -1, 0, -1 };
+
+
+    int currentStage = 0;
+
+    private void Start()
+    {
+        stageUI.UpdateUI(currentStage);
+
+    }
+
+    public void OnNextButton()
+    {
+        currentStage++;
+        
+        //UIに反映
+        stageUI.UpdateUI(currentStage);
+
+        if(encountTable.Length <= currentStage)
+        {
+            Debug.Log("clear!");
+        }
+
+        else if (encountTable[currentStage] == 0)
+        {
+            Debug.Log("敵に遭遇");
+
+        }
+
+
+    }
+
+   
+}
---
+++ wordgame/wordgame/Assets/Scripts/StageUIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//ステージのUI(ステージ数、next、back）
+
+public class StageUIManager : MonoBehaviour
+{
+    public Text stageText;
+
+    public void UpdateUI(int currentStage)
+    {
+        stageText.text = string.Format("Stage: {0}", currentStage+1);
+
+
+    }
+    
+   
+}
